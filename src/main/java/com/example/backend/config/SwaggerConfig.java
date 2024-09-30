b51deabade85a601dec9d72e693ceb8668c53c4d
Gina Lin <ginalin@alum.ccu.edu.tw>
2024-09-30 22:16:56 +0800

    add swagger support

---
+++ src/main/java/com/example/backend/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.example.backend.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API for Book Rental")
+                        .description("SpringBoot 3.x application")
+                        .version("1.0.0"));
+    }
+}
